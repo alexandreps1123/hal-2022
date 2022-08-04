3bda9c917666fc58ce4203e3d6854d248ca94c4a
alexandreps1123 <alexandref@stf.jus.br>
2022-08-04 10:20:50 -0300

    alteracao do segundo laco para rodar de 0 ate 10.

--- Mentoria01/TabuadaAutomatica/src/ProcessaTabuada.java
+++ Mentoria01/TabuadaAutomatica/src/ProcessaTabuada.java
@@ -4,7 +4,7 @@ public class ProcessaTabuada {
 	public static void main(String[] args) {
 		for(int i = 1; i <= 9; ++i ) {
 			System.out.printf("Tabuada de %d:\n", i);
-			for(int j = 1; j <= 9; ++j) {
+			for(int j = 0; j <= 10; ++j) {
 				System.out.printf("%d x %d = %d\n", i, j, i*j);
 			}
 			System.out.printf("\n");
