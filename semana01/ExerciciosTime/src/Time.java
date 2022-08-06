4a4ba21ae34e2e6f78546e06067fdeae3992c037
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-08-06 19:24:13 -0300

    classe Time com atributos hora, minuto, segundo e metodos personalizados setTime, exibirHoraPadrao e exibirHoraUniversal.

--- semana01/ExerciciosTime/src/Time.java
+++ semana01/ExerciciosTime/src/Time.java
@@ -1,4 +1,28 @@
 
 public class Time {
-
+	private int hora;
+	private int minuto;
+	private int segundo;
+	
+	public void setTime(int hora, int minuto, int segundo) {
+		this.hora = hora;
+		this.minuto = minuto;
+		this.segundo = segundo;
+	}
+	
+	public String exibirHoraUniversal() {
+		return this.hora + ":" + this.minuto + ":" + this.segundo;
+	}
+	
+	public String exibirHoraPadrao() {
+		if(this.hora >= 0 && this.hora < 12) {
+			return this.hora + ":" + this.minuto + ":" + this.segundo + " AM";
+		}
+		else if (this.hora == 12) {
+			return (this.hora) + ":" + this.minuto + ":" + this.segundo + " PM";
+		}
+		
+		return (this.hora-12) + ":" + this.minuto + ":" + this.segundo + " PM";
+	}
+	
 }
